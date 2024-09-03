ddddbf6cf6d11b855887183c520a3c47c702eaa8
HANMINUOOK <dnrals7929@naver.com>
2024-09-03 21:47:15 +0900

    vue study

---
+++ 03vue/vuetest1/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+   <NavbarView />
+   <CardView v-bind:data="data" @LikeCount="increseLike" @ViewCard="modalOpen" />
+   <ModalView :data="data" :isModal="isModal" :num="selectedNum" @closeModal="isModal = false" />
+</template>
+
+<script>
+import mdata from './assets/mdata';
+import NavbarView from './components/NavbarView.vue';
+import ModalView from './components/ModalView.vue';
+import CardView from './components/CardView.vue';
+
+export default {
+   name: 'appView',
+   data() {
+      return {
+         data: mdata,
+         isModal: false,
+         selectedNum: 0,
+      };
+   },
+   methods: {
+      increseLike(i) {
+         console.log(i);
+         this.data[i].like += 1;
+      },
+      modalOpen(num) {
+         this.isModal = true;
+         this.selectedNum = num;
+         console.log('view', num);
+      },
+      closeM() {
+         this.isModal = false;
+      },
+   },
+   components: {
+      NavbarView: NavbarView,
+      ModalView: ModalView,
+      CardView: CardView,
+   },
+};
+</script>
+
+<style lang="scss">
+$radius: 5px;
+</style>
---
+++ 03vue/vuetest1/src/components/CardView.vue
@@ -0,0 +1,109 @@
+<template>
+   <div class="container">
+      <h2 class="mb-10">HOT & NEW</h2>
+      <div class="cardWrap">
+         <div class="card" v-for="(item, i) in data" v-bind:key="i">
+            <div class="imgWrap">
+               <img :src="`./images/${item.img}`" :alt="`${item.img}`" />
+               <div class="hot" v-if="item.hoticon">HOT</div>
+            </div>
+            <div class="cardBody">
+               <h4>{{ item.title }}</h4>
+               <p>❤️ {{ item.num }}</p>
+               <p>대여 {{ item.won }}</p>
+               <p>👍 {{ item.like }} <button v-on:click="$emit('LikeCount', i)">클릭</button></p>
+            </div>
+            <div class="btn btn-primary" v-on:click="$emit('ViewCard', i)">상세보기</div>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+   props: {
+      data: Array,
+   },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+   width: 1000px;
+   margin: 0 auto;
+
+   @media screen and (max-width: 790px) {
+      width: 100%;
+      padding: 0 16px;
+   }
+   img {
+      display: block;
+   }
+}
+
+.cardWrap {
+   display: flex;
+   flex-wrap: wrap;
+   gap: 10px;
+   .card {
+      width: calc((100% - 40px) / 5);
+      // width: 25%;
+
+      @media screen and (max-width: 790px) {
+         width: calc((100% - 10px) / 2);
+      }
+
+      @media screen and (max-width: 560px) {
+         width: 100%;
+      }
+
+      .imgWrap {
+         width: 100%;
+         position: relative;
+         overflow: hidden;
+         cursor: pointer;
+         border-radius: 5px;
+         img {
+            width: 100%;
+            transition: 0.3s;
+         }
+
+         &:hover img {
+            transform: scale(1.1);
+         }
+         .hot {
+            position: absolute;
+            top: 10px;
+            left: 10px;
+            background-color: skyblue;
+            padding: 2px 10px;
+            border-radius: 5px;
+            font-weight: bold;
+            color: white;
+         }
+      }
+   }
+}
+.mb-10 {
+   margin-bottom: 10px !important;
+}
+
+.btn {
+   background: pink;
+   border-radius: 5px;
+   padding: 5px 16px;
+   text-align: center;
+   cursor: pointer;
+   color: white;
+   border: 0 none;
+   display: block;
+   width: 100%;
+
+   &.btn-primary {
+      background-color: skyblue;
+   }
+   &.btn-info {
+      background-color: greenyellow;
+   }
+}
+</style>
